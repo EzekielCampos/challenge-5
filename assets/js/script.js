0425537eb362d8411176e20539885f62c27e4f8c
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-05-25 23:11:29 -0700

    added modal form to html, styling of modal to css file, and modal jquery in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,30 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+let modal;
+
+$("#due-date").datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
+ 
+    modal = $( "#dialog-form" ).dialog({
+      autoOpen: false,
+      height: 400,
+      width: 350,
+      modal: true,
+      buttons: {
+        "Add Task":function(){modal.dialog("close")},
+        Cancel: function() {
+          modal.dialog( "close" );
+        }
+      },
+      close: function() {
+        modal.dialog( "close" );
+
+      }
+    });
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
@@ -34,5 +58,9 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+
+    $( ".btn" ).button().on( "click", function() {
+        modal.dialog( "open" );
+      });
 
 });
