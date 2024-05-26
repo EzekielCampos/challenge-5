6176a394004c4e2669ae1a98117e024d3ca195a7
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-05-26 13:32:29 -0700

    created task card for each taks to be rendered and a temporary taskId function

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,42 +28,70 @@ $("#due-date").datepicker({
         }
       },
       close: function() {
-
         taskInput.val("");
         dateInput.val("");
         descriptionInput.val("");
         modal.dialog( "close" );
-
       }
     });
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
+    let randomId;
+    for(let index = 0; index < 5; index++){
+
+        randomId = Math.floor(Math.random()*10000);
+
+    }
+    return randomId;
+
+
 }
+
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    
+    let taskCard = $('<div>').addClass("card project-card draggable my3").attr("data-project-id", task.id);
+    let headerEl = $("<header>").addClass("card-header h4").text(task.title);
+    let bodyEl = $("<body>").addClass("card-body");
+    let pDescriptionEl = $("<p>").addClass("card-text").text(task.description);
+    let pDateEl = $("<p>").addClass("card-text").text(task.date);
+    let cardDeleteBtn = $("<button>").addClass("btn btn-danger delete").text("delete").attr("data-project-id", task.id);
+
+    bodyEl.append(pDescriptionEl, pDateEl, cardDeleteBtn);
+
+    taskCard.append(headerEl, bodyEl);
+    return taskCard;
+
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
+
+
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+
+
 
 }
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
 
+
+
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+
 
 }
 
